cc3ce62de2b9103080c1a4367caa602278d8f5df
Igor <makowskiigor@gmail.com>
2023-01-05 21:36:31 +0100

    feat(components): implement text card

---
+++ components/text-card.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card color="background-600">
+    <div class="flex flex-col gap-4 p-2">
+      <v-card-title class="text-h4 !p-4"><slot name="header" /></v-card-title>
+
+      <v-card-text class="flex flex-col gap-2">
+        <slot />
+      </v-card-text>
+    </div>
+  </v-card>
+</template>
