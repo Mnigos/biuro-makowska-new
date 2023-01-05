b43b22103417473630885b2bb38f83e13a42f461
Igor <makowskiigor@gmail.com>
2023-01-05 21:35:35 +0100

    feat(layouts): create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <v-app class="bg-background-800 !min-h-screen">
+      <v-layout>
+        <div
+          class="bg-no-repeat !min-h-screen flex flex-col max-w-screen"
+          :style="
+            route.path === '/' ? 'background-image: url(/background.jpg)' : ''
+          "
+        >
+          <navigation-bar />
+
+          <v-main>
+            <div class="p-4">
+              <slot />
+            </div>
+          </v-main>
+
+          <the-footer class="justify-self-end" />
+        </div>
+      </v-layout>
+    </v-app>
+  </div>
+</template>
