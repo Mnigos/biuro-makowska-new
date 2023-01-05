302fd9877d3f26d9d0da6d281e3b86ccbf95958c
Igor <makowskiigor@gmail.com>
2023-01-05 21:36:00 +0100

    feat(components): implement footer

---
+++ components/the-footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="flex items-end">
+    <v-card class="w-screen" color="primary">
+      <v-card-text class="flex justify-center">
+        Igor Makowski {{ new Date().getFullYear() }} &copy; All right reserved.
+      </v-card-text>
+    </v-card>
+  </footer>
+</template>
