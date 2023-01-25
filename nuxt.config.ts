aa048995da3fd520765fa9b841c5b6beef5ac5cf
Mnigos <makowskiigor@gmail.com>
2023-01-25 13:25:24 +0100

    feat(config): configure graphql client and create queries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,24 @@
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', 'nuxt-windicss'],
+  modules: ['@pinia/nuxt', 'nuxt-windicss', 'nuxt-graphql-client'],
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.css',
   ],
   build: {
     transpile: ['vuetify'],
+  },
+  'graphql-client': {
+    clients: {
+      default: {
+        host: `${process.env.NUXT_API_STRAPI_URL}/graphql`,
+        token: {
+          name: 'Authorization',
+          type: 'Bearer',
+          value: process.env.NUXT_API_STRAPI_TOKEN,
+        },
+        retainToken: true,
+      },
+    },
   },
   vite: {
     define: {
