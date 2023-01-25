ba5aeae05cff7d0115c133adc263af015aa12631
Mnigos <makowskiigor@gmail.com>
2023-01-25 13:54:57 +0100

    feat(pages/index): connect with strapi

--- components/text-card.vue
+++ components/text-card.vue
@@ -1,10 +1,27 @@
+<script setup lang="ts">
+import { marked } from 'marked'
+
+defineProps<{
+  content?: string
+}>()
+</script>
+
 <template>
+  <!-- eslint-disable vue/no-v-html -->
   <v-card color="background-600">
     <div class="flex flex-col gap-4 p-2">
       <v-card-title class="text-h4 !p-4"><slot name="header" /></v-card-title>
 
-      <v-card-text class="flex flex-col gap-2">
-        <slot />
+      <v-card-text>
+        <div
+          v-if="content"
+          class="flex flex-col gap-2 list-disc list-inside"
+          v-html="marked.parse(content)"
+        />
+
+        <div v-else class="flex flex-col gap-2">
+          <slot />
+        </div>
       </v-card-text>
     </div>
   </v-card>
