e84574e7b959acc341ff3f568a943444b5989f33
Mnigos <makowskiigor@gmail.com>
2023-01-25 13:54:57 +0100

    feat(pages/index): connect with strapi

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import { getAboutCompanyData, getWelcomeToMyPageData } from '~/api'
+
 const email = ref('')
 const phone = ref('')
+
+const aboutCompanyData = await getAboutCompanyData()
+const welcomeToMyPageData = await getWelcomeToMyPageData()
 
 onMounted(() => {
   email.value = 'biuro.makowska@gmail.com'
@@ -23,23 +28,11 @@ onMounted(() => {
         <img :src="'/logo.png'" alt="Biuro Rachunkowe" class="mt-36 sm:mt-0" />
       </header>
 
-      <text-card class="lg:mt-48 md:w-128">
-        <template #header> Witam na mojej stronie </template>
-
-        <p>
-          Proponuję Państwu profesjonalne usługi księgowe w atrakcyjnych cenach
-          z możliwością odbioru dokumentów od klienta.
-        </p>
-
-        <div>
-          <p class="font-semibold">Posiadam:</p>
-
-          <ul class="list-disc px-4">
-            <li>wieloletnie doświadczenie w obsłudze księgowej firm</li>
-            <li>licencję Ministra Finansów</li>
-            <li>ubezpieczenie w zakresie odpowiedzialności cywilnej</li>
-          </ul>
-        </div>
+      <text-card
+        class="lg:mt-48 md:w-128"
+        :content="(welcomeToMyPageData.content as string)"
+      >
+        <template #header> {{ welcomeToMyPageData.header }} </template>
       </text-card>
     </article>
 
@@ -53,44 +46,11 @@ onMounted(() => {
           class="md:w-1/2 xl:w-1/3"
         />
 
-        <text-card class="lg:w-1/2 lg:mt-20">
-          <template #header>O Firmie</template>
-
-          <p>
-            Biuro Rachunkowe Joanna Makowska to certyfikowane biuro, które
-            zapewnia swoim klientom profesjonalną obsługę oraz rzetelne i
-            fachowe świadczenie usług księgowych.
-          </p>
-
-          <p>
-            Wieloletnia praca zawodowa, nabyta wiedza teoretyczna i praktyczna
-            oraz umiejętności jej wykorzystania to dowód na rzetelną realizację
-            usług.
-          </p>
-
-          <p>
-            Działalność biura rachunkowego to dziś istotna pomoc dla wielu
-            przedsiębiorców. Chcemy wychodzić naprzeciw wymaganiom i
-            oczekiwaniom, które pojawiają się ze strony przedsiębiorców, dlatego
-            oferujemy szeroki zakres usług księgowych i kadrowych w ramach
-            prowadzenia KPiR, ksiąg rachunkowych, obsługi płac pracowniczych,
-            deklaracji podatkowych, ZUS, kontaktów z Urzędem Skarbowy, rozliczeń
-            rocznych PIT oraz pomocy przy działalności gospodarczej.
-          </p>
-
-          <p>
-            Świadczone przez nas usługi księgowe i kadrowe realizujemy na
-            podstawie certyfikatu księgowego wydanego przez Ministra Finansów nr
-            26183/01. Posiadamy obowiązkowe i dodatkowe ubezpieczenie OC.
-          </p>
-
-          <p>
-            Jednocześnie stale poszerzamy swoją merytoryczną wiedzę uczestnicząc
-            na bieżąco w organizowanych szkoleniach, chociażby w
-            Stowarzyszeniach Księgowych w Polsce. Serdecznie zapraszamy Państwa
-            do skorzystania z naszych kompleksowych i elastycznie dopasowanych
-            do Państwa potrzeb usług.
-          </p>
+        <text-card
+          class="lg:w-1/2 lg:mt-20"
+          :content="(aboutCompanyData.content as string)"
+        >
+          <template #header>{{ aboutCompanyData.header }}</template>
         </text-card>
       </section>
     </article>
